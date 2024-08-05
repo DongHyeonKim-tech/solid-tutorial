0142100c05c5a8844bba1dbbd82c0c4b0103c268
김동현 <71127040+DongHyeonKim-tech@users.noreply.github.com>
2024-08-05 15:11:10 +0900

    [추가] navigation 기능 추가

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,9 +1,13 @@
 import { Component } from "solid-js";
+import { A } from "@solidjs/router";
 
 const About: Component = () => {
   return (
     <div>
       <h1>ABOUT</h1>
+      <nav>
+        <A href={"/"}>Home</A>
+      </nav>
     </div>
   );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,16 @@
 import { Component } from "solid-js";
 import logo from "@/logo.svg";
 import styles from "@/App.module.css";
+import { A } from "@solidjs/router";
 
 const Home: Component = () => {
   return (
     <div class={styles.App}>
       <header class={styles.header}>
         <img src={logo} class={styles.logo} alt="logo" />
+        <nav>
+          <A href={"/about"}>About</A>
+        </nav>
         <p>
           Edit <code>src/App.tsx</code> and save to reload!!!!
         </p>
